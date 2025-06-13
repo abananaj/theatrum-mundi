2cba04d1a23cae8430d41e4b1dc04adbf0ead3b4
abananaj <anna@theatrum.dev>
2025-06-13 00:50:00 -0700

    add bootsrap navbar

--- development/index.js
+++ development/index.js
@@ -9,4 +9,4 @@ console.log(testModule);
 // BABEL TEST
 [1, 2, 3].map(n => n + 1);
 
-// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
