cfa946ca379e082d5d48d8b1001d0bb8a955940a
abananaj <anna@theatrum.dev>
2025-06-16 02:39:52 -0700

    remove shit

---
+++ dev/index.js
@@ -0,0 +1,21 @@
+// ==================== TESTS
+console.log('hello from index.js');
+// module
+import { testModule } from "../assets/scripts/js/testModule";
+console.log(testModule);
+// babel
+[1, 2, 3].map(n => n + 1);
+
+
+// ==================== STYLES
+import "./index.scss";
+
+// ==================== NPM IMPORTS
+import { gsap } from "gsap";
+import { bootstrap } from "bootstrap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import { LocomotiveScroll } from "locomotive-scroll";
+
+// ==================== CUSTOM MODULES
+import { nameMorph } from '../assets/scripts/js/homepg';
