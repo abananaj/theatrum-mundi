9ff56cb1f85fc539f4ee10c6d468c4b622515de2
abananaj <anna@theatrum.dev>
2025-06-16 15:50:47 -0700

    gulpfile.ts update - only handling HTML

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,28 +1,8 @@
 const { src, dest, watch, series, parallel } = require("gulp");
-// const sass = require("gulp-sass")(require("sass"));
-// const babel = require("gulp-babel");
 const html = require("gulp-file-include");
 
-// function compileStyles() {
-//   return src("dev/index.scss")
-//     .pipe(sass({
-//       silenceDeprecations: ['import']
-//     }))
-//     .pipe(dest("prod"));
-// }
-
-// function compileScripts() {
-//   return src("dev/index.ts")
-//     .pipe(
-//       babel({
-//         presets: ["@babel/env", "@babel/preset-typescript"],
-//       })
-//     )
-//     .pipe(dest("prod"));
-// }
-
+// CONTENT
 function compileContent() {
-  
   return src("dev/html/*.html")
     .pipe(
       html({
@@ -32,37 +12,9 @@ function compileContent() {
     )
     .pipe(dest("dev"));
 }
-
-function watchContent() {
-  watch(
-    ["dev/html/**/*.html", "dev/html/*.html"],
-    compileContent
-  );
-}
-// function watchStyles() {
-//   watch(
-//     ["dev/styles/**/*.scss", "dev/index.scss"],
-//     compileStyles
-//   );
-// }
-// function watchScripts() {
-//   watch(
-//     ["dev/scripts/**/*.js", "dev/index.js"],
-//     compileScripts
-//   );
-// }
-
-exports.compile = parallel(
-  // compileStyles,
-  // compileScripts,
-  compileContent);
-
-exports.watch = parallel(
-  // watchStyles,
-  // watchScripts,
-  watchContent);
+function watchContent() { watch(["dev/html/**/*.html", "dev/html/*.html"], compileContent); }
 
   exports.default = parallel(
-    exports.compile,
-    exports.watch
+    compileContent,
+    watchContent
   );
