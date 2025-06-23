7332b0876b459c20c0ef648f89d5a6d7af18e448
abananaj <anna@theatrum.dev>
2025-06-22 21:45:41 -0700

    looking good and working

--- dev/scripts/navbar.ts
+++ dev/scripts/navbar.ts
@@ -8,7 +8,7 @@ const navToggle = gsap
     .from("#navbar nav ul li", { xPercent: -100, duration: 0.35 });
 
 ScrollTrigger.create({
-    trigger: ".tm-scroll",
+    trigger: "body",
     start: "75% bottom",
     animation: navToggle,
     toggleActions: "play none none reverse",
